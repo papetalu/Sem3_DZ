ea1384e9eb4bdfbb84753bd84745d60327318976
Liuba <pl@mail.ru>
2022-09-24 11:30:52 +0300

    Task 3

--- ZD23/Program.cs
+++ ZD23/Program.cs
@@ -3,5 +3,10 @@ Console.Clear();
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= n; i++)
-   Console.Write(i * i + " ");
+{
+   if (i == n) Console.Write(i * i * i);
+   else
+      Console.Write(i * i * i + ", ");
+}
+
 
