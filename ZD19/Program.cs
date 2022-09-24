266c384d8d4a2e900d4d65b16f2cac61e4af4f2c
Liuba <pl@mail.ru>
2022-09-24 11:54:41 +0300

    Task 1

--- ZD19/Program.cs
+++ ZD19/Program.cs
@@ -1,5 +1,27 @@
 ﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 Console.Clear();
-Console.Write("Введите номер четверти: ");
-int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите пятизначное число: ");
+int n;
+
+while (true)
+{
+   n = Convert.ToInt32(Console.ReadLine());
+   if (n >= 10000 && n < 100000)
+   {
+      break;
+   }
+   Console.WriteLine("Число не пятизначное. Попробуйте снова");
+}
+
+string a = Convert.ToString(n);
+if (a[0] == a[4] && a[1] == a[3])
+{
+   Console.Write("Число " + n + " палиндром ");
+
+}
+else
+{
+   Console.Write("Число " + n + " не палиндром");
+
+}
 
