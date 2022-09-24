3c9958aa0995cf92d724236c57a445177d8406cc
Liuba <pl@mail.ru>
2022-09-24 11:24:49 +0300

    Start

---
+++ ZD23/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+   Console.Write(i * i + " ");
+
